6b6afa35df741b8f82589dab72dc46e7ea5b47b5
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2021-08-10 00:29:57 +0300

    createa singleton base

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = (T)FindObjectOfType(typeof(T));
+            }
+
+            return _instance;
+        }
+    }
+
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = (T)this;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (_instance == this)
+        {
+            _instance = null;
+        }
+    }
+
+}
