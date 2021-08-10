03b6517541845ae5e800a690f50102bb02e33802
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2021-08-10 22:55:13 +0300

    create UIManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,79 +11,29 @@ public enum GameState
 
 public class GameManager : Singleton<GameManager>
 {
-    private float _collected;
-    private float _total;
-    private GameState _gameStatus;
-    public float Total { get => _total; }
-    public float Collected { get => _collected; }
-    public GameState GameStatus { get => _gameStatus; }
+    float _collected;
+    float _total;
+    GameState _gameStatus;
 
-    [SerializeField] GameObject _congratsPanel;
-    [SerializeField] Slider _slider;
-    [SerializeField] GameObject _nextLevelBG;
-    Color _defaultColor = new Color(0.8509804f, 0.7960784f, 0.7960784f, 1f);
-    Color _fillColor = new Color(0.7058949f, 0.8584906f, 0.1093361f, 1f);
+    public float Total => _total;
+    public float Collected => _collected;
+    public GameState GameStatus => _gameStatus;
 
-    WaitForSeconds wait = new WaitForSeconds(.001f);
     void Start()
     {
-        InitGame();
-    }
-    public void StartGame()
-    {
-        _gameStatus = GameState.Play;
-    }
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-    void InitGame()
-    {
-        SetCongratsPanel(false);
+
+        UIManager.Instance.SetCongratsPanel(false);
         _gameStatus = GameState.Play;
         _collected = 0;
         _total = FindObjectsOfType<Obstacle>().Length;
-        UpdateLevelBar();
-
+        UIManager.Instance.UpdateLevelBar(_collected, _total);
     }
 
-    public void IncreaseCollected()
-    {
-        _collected++;
+    public void StartGame() => _gameStatus = GameState.Play;
 
-        UpdateLevelBar();
-    }
+    public void RestartGame() =>SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    public void IncreaseCollected() => _collected++;
+    public void SetGameStatus(GameState gameState) => _gameStatus = gameState;
 
-    void UpdateLevelBar()
-    {
-        _slider.value = (_collected / _total) * 100f;
-
-        if (_slider.value == 100f)
-        {
-            //set color;
-            _nextLevelBG.GetComponent<Image>().color = _fillColor;
-
-            //SetCongratsPanel(true);
-            _gameStatus = GameState.PreFinish;
-
-            //say player make radius 1-to-10
-            //rotate ground obj 
-            //
-            //FinishGameAnim();
-
-
-        }
-        else
-        {
-            _nextLevelBG.GetComponent<Image>().color = _defaultColor;
-        }
-    }
-
-    void SetCongratsPanel(bool isShow)
-    {
-        _congratsPanel.SetActive(isShow);
-    }
-
-
-    
 }
+
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singleton<UIManager>
+{
+    [SerializeField] Color _color;
+    [SerializeField] Slider _slider;
+    [SerializeField] GameObject _congratsPanel;
+    [SerializeField] Image _nextLevelBGImage;
+
+
+    Color _defaultColor = new Color(0.8509804f, 0.7960784f, 0.7960784f, 1f);
+    Color _fillColor = new Color(0.7058949f, 0.8584906f, 0.1093361f, 1f);
+    
+
+
+    public void UpdateLevelBar(float collected, float total)
+    {
+        _slider.value = (collected / total) * 100f;
+
+        if (_slider.value == 100f)
+        {
+            
+            _nextLevelBGImage.color = _fillColor;
+            SetCongratsPanel(true);
+            GameManager.Instance.SetGameStatus(GameState.PreFinish);
+
+            //say player make radius 1-to-10
+            //rotate ground obj 
+
+            //FinishGameAnim();
+        }
+        else
+        {
+            _nextLevelBGImage.color = _defaultColor;
+        }
+    }
+
+    public void SetCongratsPanel(bool isShow) => _congratsPanel.SetActive(isShow);
+
+}
