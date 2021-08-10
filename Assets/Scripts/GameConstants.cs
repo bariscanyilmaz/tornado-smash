f9a17b106135597ef11b542427e3a039ee1108b7
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2021-08-10 21:20:48 +0300

    add game constants

---
+++ Assets/Scripts/GameConstants.cs
@@ -0,0 +1,8 @@
+﻿public static class GameConstants
+{
+    public const string HorizontalAxisName="Horizontal";
+    public const string VerticalAxisName="Vertical";
+    public const string ObstacleTagName="Obstacle";
+    public const string GroundTagName="Ground";
+
+}
