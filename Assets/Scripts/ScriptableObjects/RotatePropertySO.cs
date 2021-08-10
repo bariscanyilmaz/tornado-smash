506152b7d4eb0110c95c2cd3590ed8e51631e890
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2021-08-10 03:28:08 +0300

    create scriptable objects for tornado,ground and obstacle objects

---
+++ Assets/Scripts/ScriptableObjects/RotatePropertySO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "RotatePropertyData", menuName = "ScriptableObjects/RotateProperyData", order = 2)]
+public class RotatePropertySO : ScriptableObject
+{
+    public float RotateSpeed=360f;
+    public float RiseSpeed=5f;
+    public float ScaleSpeed=0.5f;
+
+    public float RadiusReduceSpeed=30f;
+}
---
+++ Assets/Scripts/ScriptableObjects/TornadoPropertySO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "TornadoPropertyData", menuName = "ScriptableObjects/TornadoProperyData", order = 1)]
+public class TornadoPropertySO : ScriptableObject
+{
+    public float MaxDistance=7f;
+    public float MinDistance=3f;
+    public float Damper=0f;
+    public float SpringForce=50f;
+    public float Force=50f;
+    public float MaxRadius = 35f; 
+    public float RadiusReduceSpeed=30f;  
+}
