479e176947e3a837a3d597c1a6e7c4657c498ae3
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2021-08-10 22:00:32 +0300

    add plane component

---
+++ Assets/Scripts/Plane.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+public class Plane : MonoBehaviour
+{
+    [SerializeField] float _maxWidth,_maxHeight;
+
+    public float MaxWidth=>_maxWidth;
+    public float MaxHeight=>_maxHeight;
+
+}
